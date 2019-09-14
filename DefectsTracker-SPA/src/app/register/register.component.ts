5c8669e4c3cbfc868fd3f3db9986dca689267d43
realbenleong <realbenleong@gmail.com>
2019-09-14 17:40:15 +0800

    Final

--- DefectsTracker-SPA/src/app/register/register.component.ts
+++ DefectsTracker-SPA/src/app/register/register.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { AuthService } from '../_services/auth.service';
+import { AlertifyService } from '../_services/alertify.service';
 
 @Component({
   selector: 'app-register',
@@ -10,16 +11,18 @@ export class RegisterComponent implements OnInit {
   @Input() valuesFromHome: any;
   model: any = {};
 
-  constructor(private authService: AuthService) { }
+  constructor(private authService: AuthService, private alertify: AlertifyService) { }
 
   ngOnInit() {
   }
 
   register() {
     this.authService.register(this.model).subscribe(() => {
-      console.log('registration success.');
+      console.log('Registration successful.');
+      this.alertify.success('Registration successful.');
     }, error => {
       console.log(error);
+      this.alertify.error('Registration unsuccessful.');
     });
   }
 }
